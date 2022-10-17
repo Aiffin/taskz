2335198b74d037bfcfb97a34c31e7742f0080115
Aiffin <aiffinsk@gmail.com>
2022-10-17 16:19:31 +0530

    update Task and delete

--- controller/taskController.js
+++ controller/taskController.js
@@ -4,7 +4,7 @@ const taskController = {
     index:async(req,res)=>{
 
         const taskList = await Task.find({})
-        console.log('tasks:',taskList)
+        // console.log('tasks:',taskList)
 
         res.render('index',{tasks:taskList})
     },
@@ -12,7 +12,11 @@ const taskController = {
         res.render('create')
     },
     edit:async(req,res)=>{
-        res.render('update')
+        const id = req.params.id // to read ref id from router
+
+        const data = await Task.findById({_id:id})
+        // console.log('single data=',data)
+        res.render('update',{task:data})
     },
     createTask:async (req,res) =>{
         // console.log('data=',req.body)
@@ -22,6 +26,19 @@ const taskController = {
         // console.log('data:',newTask)
         newTask.save()
         console.log('TASK CREATED SUCCESSFULLY')
+        res.redirect(`/`)
+    },
+    updateTask:async(req,res)=>{
+        const data=req.body
+        // console.log('update data=',data)
+        await Task.findByIdAndUpdate({_id:req.params.id},data)
+        console.log('task update')
+        res.redirect(`/`)
+    },
+    deleteTask:async(req,res)=>{
+        const id= req.params.id
+        await Task.findByIdAndDelete({_id:id})
+        console.log('task deleted')
         res.redirect(`/`)
     }
 
